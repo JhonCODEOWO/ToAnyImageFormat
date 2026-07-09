2f08f24185be10dcf69c020dad60f4b0eb3f98bc
Jonathan Juárez Valera (Laptop) <jjv20618@gmail.com>
2026-07-09 14:15:14 -0600

    refactor: Docs added and a method to add the extension in the TransformedImage object.

--- core/Transformator.php
+++ core/Transformator.php
@@ -2,19 +2,27 @@
 
 namespace Core;
 
-use Functions;
 use Intervention\Image\Drivers\Imagick\Driver;
 use Intervention\Image\ImageManager;
-use Intervention\Image\Interfaces\DriverInterface;
 use Intervention\Image\Interfaces\EncodedImageInterface;
-use Intervention\Image\Interfaces\ImageInterface;
+use Utils\Functions;
 
 class Transformator {
 
     private string $uniqueName;
     private ImageManager $imageManager;
     private ?TransformedImage $imageTransformed =  null;
-
+    
+    /**
+     * __construct Initializes a transformator with the options to apply in its transformations.
+     *
+     * @param  string $tmpPath Path to the image file to transform.
+     * @param  string $clientName Original name of the image file.
+     * @param  int $quality Quality to apply in the transformations.
+     * @param  bool $keepOriginalName Flag to indicate if the name of the result 
+     *  should persists with the `$clientName` given.
+     * @return void
+     */
     public function __construct(private string $tmpPath, string $clientName,private ?int $quality = 80,?bool $keepOriginalName = false)
     {
         $this->tmpPath = $tmpPath;
@@ -30,22 +38,38 @@ class Transformator {
     public function toWebp(): static{
         $imageTransformed = $this->readImage()->toWebp($this->quality);
 
-        $this->setTransformedImage($imageTransformed);
+        $this->setTransformedImage($imageTransformed, 'webp');
 
         return $this;
     }
 
     public function toPng(): static{
-        $imageTransformed = $this->readImage()->toPng($this->quality);
+        $imageTransformed = $this->readImage()->toPng();
 
-        $this->setTransformedImage($imageTransformed);
+        $this->setTransformedImage($imageTransformed, 'png');
         return $this;
     }
 
-    private function setTransformedImage(EncodedImageInterface $encodedImage){
+    private function setTransformedImage(EncodedImageInterface $encodedImage, string $extension){
+        $this->setNameExtension($extension);
         $this->imageTransformed = new TransformedImage($this->uniqueName, $encodedImage);
     }
-
+    
+    /**
+     *  Sets the name with the extension given.
+     *
+     * @param  string $extension A valid extension to the file.
+     * @return void
+     */
+    private function setNameExtension(string $extension) {
+        $this->uniqueName = $this->uniqueName.".$extension";
+    }
+    
+    /**
+     *  Returns the current state of imageTransformed property.
+     *
+     * @return \Core\TransformedImage | null
+     */
     public function getTransformedImage(){
         return $this->imageTransformed;
     }
