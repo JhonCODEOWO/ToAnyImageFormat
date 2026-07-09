c0798cd3b90c2cf080dda5d2cb7f90839acce02b
Jonathan Juárez Valera (Laptop) <jjv20618@gmail.com>
2026-07-09 14:16:17 -0600

    feat: Adds debug function.

--- utils/Functions.php
+++ utils/Functions.php
@@ -1,7 +1,16 @@
 <?php
 
+namespace Utils;
+
 class Functions {
     static function getNameWithoutFormat(string $fileName): string{
         return explode('.', $fileName)[0];
+    }
+
+    static function debug($input) {
+        echo '<pre>';
+        var_dump($input);
+        echo '</pre>';
+        exit;
     }
 }
