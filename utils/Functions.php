a5059594bfeae51c6ec0c98ee155e80c85bdde82
Jonathan Juárez Valera <jjv20618@gmail.com>
2026-07-09 15:36:45 -0600

    feature: Adds function to retrieve a forced download to the browser.

--- utils/Functions.php
+++ utils/Functions.php
@@ -2,6 +2,8 @@
 
 namespace Utils;
 
+use Error;
+
 class Functions {
     static function getNameWithoutFormat(string $fileName): string{
         return explode('.', $fileName)[0];
@@ -11,6 +13,24 @@ class Functions {
         echo '<pre>';
         var_dump($input);
         echo '</pre>';
+        exit;
+    }
+
+    static function downloadTemporaryFileToClient(string $path, string $mimeType, string $fileName) {
+        if(!file_exists($path)) 
+            throw new Error("Can't download a file that doesn't exists");
+
+
+        header("Content-Type: $mimeType");
+        header("Content-Disposition: attachment; filename=$fileName");
+        header('Content-Length: ' . filesize($path));
+        header('Pragma: public');
+        header('Cache-Control: must-revalidate');
+
+        readfile($path);
+
+        unlink($path);
+
         exit;
     }
 }
